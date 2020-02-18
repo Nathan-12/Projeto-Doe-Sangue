ee408c43c8ab52cad881b2e7b2243925c32d9d99
Nathan Lima <nathanlima.b@gmail.com>
2020-02-18 10:44:39 -0300

    Finalização das requisições back

--- ProjectDoe-front/server.js
+++ ProjectDoe-front/server.js
@@ -5,6 +5,19 @@ const server = express()
 //Configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'))
 
+//Habilitar body do form
+server.use(express.urlencoded({ extended: true}))
+
+// COnfigurar a conexão com o BD
+const Pool = require("pg").Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    post: 5432,
+    database: 'doe'
+})
+
 //Configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -13,6 +26,7 @@ nunjucks.configure("./", {
 })
 
 //Lista de doadores -> Vetor ou Array
+/*
 const donors = [
     {
         name: "Nathan Lima",
@@ -26,12 +40,46 @@ const donors = [
         name: "Maria de Fátima",
         blood: "A+"
     }
-]
+] */
 
 
 //Configurar a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html", { donors })
+   
+    db.query("SELECT * FROM donors", function(err, result){
+        if(err) return res.send("Erro de bando de dados.")
+
+        const donors = result.rows
+
+        return res.render("index.html", { donors })
+    })
+})
+
+
+
+server.post("/", function(req, res) {
+    //pegar dados do form
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    if (name == "" || email == "" || blood == "" ){
+        return res.send("Todos os campos são obrigatórios.")
+    }
+
+    // Coloca os valores dentro do bd
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+    
+    db.query(query, values, function(err){
+        if(err) return res.send("Erro no banco de dados.")
+
+        return res.redirect("/")
+    })
+
+    
+
 })
 
 //Ligar o servidor e permitir o acesso na porta 3000
